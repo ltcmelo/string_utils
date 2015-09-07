2e9a935d60db8d6d4d008a17d4e4e28d1cc39ff3
Leandro T. C. Melo <ltcmelo@gmail.com>
2015-09-06 22:17:58 -0300

    Update copyright year

--- config.hpp
+++ config.hpp
@@ -1,7 +1,7 @@
 /******************************************************************************
  The MIT License
 
- Copyright (c) since 2009 Leandro T. C. Melo
+ Copyright (c) 2009-2015 Leandro T. C. Melo
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
